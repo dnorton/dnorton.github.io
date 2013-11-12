6463c2f42ead4328c34a32b766d01dddd577cefc
Daniel Norton <daniel@dnorton.org>
2013-11-11 22:10:58 -0500

    added README section for debugging

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 /**
  * Created with IntelliJ IDEA.
- * User: dnorton
- * Date: 11/11/13
- * Time: 9:35 PM
- * To change this template use File | Settings | File Templates.
+ *
  */
 var connect = require('connect');
 
